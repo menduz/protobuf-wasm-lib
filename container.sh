f7b581e2064084eb98ae3ecd3cdbd80ce0064141
Agustin Mendez <agustin.mz.92@gmail.com>
2020-05-07 14:53:44 -0300

    cleanup cached image

--- container.sh
+++ container.sh
@@ -6,6 +6,6 @@ buildCmd="cd protobuf && bash -x ../build.sh"
 
 docker build . -t builder-image
 mkdir -p .data
-#docker rmi trzeci/emscripten
+
 docker run --rm -v `pwd`:/src -v $(PWD)/.data:/emsdk_portable/.data builder-image \
   bash -c "$buildCmd"
